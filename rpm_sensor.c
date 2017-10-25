6abaefebda2bc713c650de06ef40f86bee35e5dd
kuben <jakub.fojt@wp.pl>
2017-10-25 20:34:31 +0000

    Begin splitting code in several parts

---
+++ rpm_sensor.c
@@ -0,0 +1,6 @@
+#include "rpm_sensor.h"
+
+int poll_sensor(int pin_read, int pin_write)
+{
+
+}
---
+++ rpm_sensor.h
@@ -0,0 +1,6 @@
+#ifndef RPM_SENSOR
+#define RPM_SENSOR
+
+int poll_sensor(int pin_read, int pin_write);
+
+#endif
