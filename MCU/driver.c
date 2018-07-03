dcd9a9ff4962b3c28c1773a82592eb8f25308066
kuben <jakub.fojt@wp.pl>
2018-07-03 21:53:49 +0000

    Added utils.c file. In progress of fixing weird behaviour in tx funciton

--- MCU/driver.c
+++ MCU/driver.c
@@ -3,6 +3,7 @@
 #define NO_LEGACY_DEFINES
 #include <pic14regs.h>//<pic16f685.h>
 #include <stdint.h>
+#include "utils.h"
 
 // Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN),
 // disable watchdog,
@@ -16,6 +17,20 @@ __code uint16_t __at (_CONFIG) __configword = _INTRC_OSC_NOCLKOUT & _WDTE_OFF & 
 #define RX PORTBbits.RB4
 #define RX_TRIS TRISBbits.TRISB4
 
+#define REN PORTBbits.RB7
+#define RF PORTBbits.RB6
+#define RR PORTBbits.RB5
+#define LEN PORTCbits.RC4
+#define LF PORTAbits.RA4
+#define LR PORTCbits.RC5
+
+#define REN_TRIS TRISBbits.TRISB7
+#define RF_TRIS TRISBbits.TRISB6
+#define RR_TRIS TRISBbits.TRISB5
+#define LEN_TRIS TRISCbits.TRISC4
+#define LF_TRIS TRISAbits.TRISA4
+#define LR_TRIS TRISCbits.TRISC5
+
 #define NOP __asm__ ("nop")
 #define TNOP NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP
 #define NNOP NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP;NOP
@@ -26,15 +41,7 @@ int transmit(uint8_t word);
 void UART_tx();
 void UART_start_bit();
 
-// Uncalibrated delay, just waits a number of for-loop iterations
-void delay(uint16_t iterations)
-{
-    uint16_t i;
-    for (i = 0; i < iterations; i++) {
-        // Prevent this loop from being optimized away.
-        __asm__ ("nop") ;
-    }
-}
+volatile uint8_t l_speed = 0, r_speed = 0;
 
 volatile uint8_t blink = 1;
 volatile uint8_t message[20];
@@ -56,30 +63,27 @@ struct uart_struct {
     uint8_t next_rx;//0: ready, 1,2,4,8,..: waiting on bit n, -1: waiting on stop bit
     uint8_t tx_send;
     uint8_t next_tx;//0: ready, 1,2,4,8,..: waiting on bit n, -1: waiting on stop bit
-
 };
 volatile struct uart_struct uart = {0};
-
-void strcpy(uint8_t *dest, const uint8_t *src)
-{
-     while(*src) {
-        *dest = *src;
-        src++;
-        dest++;
-    }
-    *dest = *src;
-}
 
 void main(void)
 {
     uint8_t i = 0;
     setup();
 
-    strcpy(message,"Hello!\n");
+    //Both outputs forward
+    LF = 1;
+    RF = 1;
+
+    strcpy(message,"\n\nBooted!\n");
     while (1) {//Continuosly send message
+        if(message[0] == 0) continue;//Don't print empty message
         if(transmit(message[i])) continue;
         i++;
-        if (!message[i] || i == sizeof(message)) i = 0;
+        if (!message[i] || i == sizeof(message)){
+            //message[0] = 0;
+            i = 0;
+        }
     }
 }
 
@@ -117,17 +121,64 @@ void UART_rx()
     PIR1bits.TMR2IF = 0;
 }
 
-char num2char(uint8_t num)
-{
-    return 48+num;//ASCII 48 is '0'
-}
+/*
+ * Receives 8-bit commands
+ * 00sxxxxx for brake:
+ * 10sxxxxx for forward:
+ * 01sxxxxx for reverse:
+ *  s = 0 for left, 1 for right
+ *  xxxxx is 5 bit speed. 0 for duty cycle 0%, 31 for duty cycle 100%
+ * 11xxxxxx for request:
+ *   xxxxxx = 000000 for some command...
+ */
 void rx_done(uint8_t word)
 {
     strcpy(message, "Received:   [    ]!\n");
     message[10] = word;
+    num2str(word,message+13,0);
     message[13] = num2char(word/100);
     message[14] = num2char((word%100)/10);
     message[15] = num2char(word%10);
+
+    return;
+    if ((word & 0xc0) == 0xc0){
+        //Request
+        return;
+    }
+    //If not request then set speed
+    if (word & 0x20){//Right
+        switch (word & 0xe0){
+            case 0x00://Brake
+                RF = 0;
+                RR = 0;
+                break;
+            case 0x80://Forward
+                RF = 1;
+                RR = 0;
+                break;
+            case 0x40://Reverse
+                RF = 0;
+                RR = 1;
+                break;
+        }
+        r_speed = word & 0x1f;
+    } else {//Left
+        switch (word & 0xe0){
+            case 0x00://Brake
+                LF = 0;
+                LR = 0;
+                break;
+            case 0x80://Forward
+                LF = 1;
+                LR = 0;
+                break;
+            case 0x40://Reverse
+                LF = 0;
+                LR = 1;
+                break;
+        }
+        l_speed = word & 0x1f;
+    }
 }
 
 int transmit(uint8_t word)
@@ -147,6 +198,7 @@ int transmit(uint8_t word)
 
 void UART_tx()
 {
+    uint8_t nexttx;
     if (uart.next_tx == 0xfe){//Waiting to send stop bit
         TX = 1;//Stop bit high
         uart.next_tx = 0xff;
@@ -156,8 +208,10 @@ void UART_tx()
         IOCBbits.IOCB4 = 1;//Reenable IOC for rx
     } else {//Waiting to send data bit
         //uart.next_tx = 1<<blink;
+        nexttx = (1<<blink);
         TX = (uart.tx_send & (1<<blink)) > 0;//Send data bit n
         //TX = (uart.tx_send & uart.next_tx) > 0;//Send data bit n
+        //TX = (uart.tx_send & nexttx) > 0;//Send data bit n
         //if(uart.next_tx < 0x80) uart.next_tx = uart.next_tx<<1;//Wait for next bit
         if(blink < 7) uart.next_tx = uart.next_tx<<1;//Wait for next bit
         else uart.next_tx = 0xfe;//Now wait for stop bit
@@ -204,10 +258,22 @@ void setup()
 
     //Setup Timer 2 (baud rate timing)
     T2CON = 0;
-    T2CONbits.T2CKPS = 2;
-    //T2CONbits.TOUTPS = 5;
-    //PR2 = 35-1;//114286 baud
-    //PR2 = 9-1;//111111 baud
-    PR2 = 16;//104-1;//9600 baud
+    T2CONbits.T2CKPS = 1; //1200 baud
+    PR2 = 67;
     PIE1bits.TMR2IE = 1;
+
+    //Setup Right,Left Forward, Reverse and Enable as ouputs
+    RF_TRIS = 0;
+    RR_TRIS = 0;
+    REN_TRIS = 0;
+    LF_TRIS = 0;
+    LR_TRIS = 0;
+    LEN_TRIS = 0;
+
+    RF = 0;
+    RR = 0;
+    REN = 0;
+    LF = 0;
+    LR = 0;
+    LEN = 0;
 }
